72f1515e6dbc331f231be8d6ec173e95ac3a0ee8
mjimecaceres <85838338+mjimecaceres@users.noreply.github.com>
2022-11-01 03:37:38 -0300

    Add files via upload

---
+++ BackEnd/mjc/src/main/java/com/portfolio/mjc/MjcApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjcApplication.class, args);
+	}
+
+}
---
+++ BackEnd/mjc/src/test/java/com/portfolio/mjc/MjcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mjc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
